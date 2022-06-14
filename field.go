df7abdcc1f7a62c7b2b9f95f7e74981b622996d8
Viacheslav Poturaev <vearutop@gmail.com>
2022-06-15 00:40:04 +0200

    Add some code changes

--- field.go
+++ field.go
@@ -13,10 +13,18 @@ type DeferredString func() interface{}
 
 // String implements fmt.Stringer.
 func (d DeferredString) String() string {
+	if d == nil {
+		panic("ctxd: DeferredString is nil")
+	}
+
 	return fmt.Sprintf("%+v", d())
 }
 
 // MarshalJSON implements json.Marshaler.
 func (d DeferredJSON) MarshalJSON() ([]byte, error) {
+	if d == nil {
+		panic("ctxd: DeferredJSON is nil")
+	}
+
 	return json.Marshal(d())
 }
