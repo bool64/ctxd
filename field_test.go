7d4290762ad9aa69d27f25f4f55e77d2f1be1d9b
Viacheslav Poturaev <vearutop@gmail.com>
2022-06-15 00:54:13 +0200

    Add more tests

--- field_test.go
+++ field_test.go
@@ -9,10 +9,17 @@ import (
 
 func TestDeferredString_String(t *testing.T) {
 	assert.Equal(t, "[1 2 3]", ctxd.DeferredString(func() interface{} { return []int{1, 2, 3} }).String())
+
+	assert.Panics(t, func() { _ = ctxd.DeferredString(nil).String() })
 }
 
 func TestDeferredJSON_MarshalJSON(t *testing.T) {
 	v, err := ctxd.DeferredJSON(func() interface{} { return []int{1, 2, 3} }).MarshalJSON()
 	assert.NoError(t, err)
 	assert.Equal(t, "[1,2,3]", string(v))
+
+	assert.Panics(t, func() {
+		_, err := ctxd.DeferredJSON(nil).MarshalJSON()
+		assert.NoError(t, err) // Unreachable.
+	})
 }
